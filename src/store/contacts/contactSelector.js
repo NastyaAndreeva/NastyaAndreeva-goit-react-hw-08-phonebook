5884c50d306d014b3eee8f0a7320143618f8296c
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-09-09 17:37:55 +0300

    fixed filter

--- src/store/contacts/contactSelector.js
+++ src/store/contacts/contactSelector.js
@@ -10,8 +10,8 @@ const getVisibleContacts = createSelector(
   [getAllContacts, getFilter],
   (items, filter) => {
     const normalizedFilter = filter.toLowerCase();
-    return items.filter(({ description }) =>
-      description.toLowerCase().includes(normalizedFilter)
+    return items.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
     );
   }
 );
