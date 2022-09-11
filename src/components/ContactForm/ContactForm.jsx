e86230c567f21e2218e16b4888b98b97b331e339
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-09-11 14:37:02 +0300

    added useFormik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,94 +1,81 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 import { nanoid } from 'nanoid';
 import styled from 'styled-components';
-import * as Yup from 'yup';
 import { Box } from 'components/Box';
 import { Button } from 'components/ui/Button';
 import { toast } from 'react-toastify';
 import { useRedux } from 'hooks';
 import { contactsOperations, contactsSelectors } from 'store/contacts';
-import { useState } from 'react';
+import { useFormik } from 'formik';
 
 const Label = styled.label`
   margin-bottom: 10px;
 `;
 
-const ContactErrorMessage = styled(ErrorMessage)`
-  color: ${({ theme }) => theme.colors.alert};
-  font-size: 10px;
-`;
-
 export const ContactForm = () => {
   const [selector, dispatch] = useRedux();
   const contacts = selector(contactsSelectors.getAllContacts);
-  const [number, setNumber] = useState(null);
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      number: '380',
+    },
+    onSubmit: (values, { resetForm, setFieldValue }) => {
+      const isAlreadyAdded = dataValidation(values);
+
+      if (isAlreadyAdded) {
+        toast.error(`${values.name} is already in your contacts`);
+        return;
+      }
+
+      const contact = {
+        id: nanoid(),
+        name: values.name,
+        number: values.number,
+      };
+
+      dispatch(contactsOperations.addContact(contact));
+      resetForm();
+    },
+  });
 
   const dataValidation = data =>
     contacts.find(
       contact => contact.name.toLowerCase() === data.name.toLowerCase()
     );
 
-  const onSubmit = data => {
-    const isAlreadyAdded = dataValidation(data);
-
-    if (isAlreadyAdded) {
-      toast.error(`${data.name} is already in your contacts`);
-      return;
-    }
-
-    const contact = {
-      id: nanoid(),
-      name: data.name,
-      number,
-    };
-
-    dispatch(contactsOperations.addContact(contact));
-  };
-
-  const handleSubmit = (values, { resetForm }) => {
-    onSubmit(values);
-    resetForm();
-    setNumber('380');
-  };
-
-  const validationSchema = Yup.object({
-    name: Yup.string().max(16).required('Please, enter your name.'),
-  });
-
   return (
-    <Formik
-      initialValues={{
-        name: '',
-      }}
-      onSubmit={handleSubmit}
-      validationSchema={validationSchema}
-    >
-      <Form autoComplete="off">
-        <Box
-          width="400px"
-          margin="0 auto"
-          display="flex"
-          flexDirection="column"
-          as="section"
-        >
-          <Label htmlFor="name">
-            Name
-            <Field type="text" name="name" />
-            <ContactErrorMessage name="name" component="p" />
-          </Label>
-          <Label htmlFor="test">
-            Number
-            <PhoneInput
-              country={'ua'}
-              value={number}
-              onChange={phone => setNumber(phone)}
-            />
-          </Label>
-          <Button type="submit">Add contact</Button>
-        </Box>
-      </Form>
-    </Formik>
+    <form onSubmit={formik.handleSubmit}>
+      <Box
+        width="400px"
+        margin="0 auto"
+        display="flex"
+        flexDirection="column"
+        as="section"
+      >
+        <Label htmlFor="name">
+          Name
+          <input
+            name="name"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+        </Label>
+        <Label htmlFor="test">
+          Number
+          <PhoneInput
+            country={'ua'}
+            formik={formik}
+            onChange={e => formik.setFieldValue('number', e)}
+            name="number"
+            value={formik.values.number}
+          />
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Box>
+    </form>
   );
 };
